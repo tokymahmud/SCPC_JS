c153abcdd8fd18f4acd3ef1d68147c1fcc7ec9c5
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-07-25 16:38:08 +0600

    fixwd code

--- smallest.js
+++ smallest.js
@@ -1,20 +1,9 @@
-const findSecondSmallest = (numbersArray) => {
-    let smallest = 0;
-    let secondSmallest = 0;
-  
-    for (const num of numbersArray) {
-      if (num < smallest) {
-        secondSmallest = smallest;
-        smallest = num;
-      } else if (num < secondSmallest && num !== smallest) {
-        secondSmallest = num;
-      }
-    }
-  
-    return secondSmallest;
+const ssmall = (numbersArray) => {
+    const sortedArray = numbersArray.slice().sort((a, b) => a - b);
+    return sortedArray[1];
   };
   
-  const numbers = [5, -1, 2, 8, 1, 7, 3];
-  const secondSmallest = findSecondSmallest(numbers);
-  console.log("Second smallest number:", secondSmallest);
+  const numbers = [5,1, 2, 8, 7, 3];
+  const secondSmall = ssmall(numbers);
+  console.log("Second small number: ", secondSmall);
   
